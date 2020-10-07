b5ea08a40fea8f21a43f5d1eb9a0518c152c9870
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2020-10-08 00:44:43 +0300

    Added solution to the 4th task.

--- js/task-3.js
+++ js/task-3.js
@@ -1,3 +1,71 @@
 console.log(
   "Task 3 ------------------------------------------------------------"
 );
+
+// Task 3
+// Variant 1
+const findBestEmployee = function (employees) {
+  let justStats = Object.values(employees);
+  let bestStats = Math.max(...justStats);
+  let bestEmployeeName = "";
+
+  // if (Object.keys(employees).length === 0) {
+  //   return "";
+  // }
+
+  for (const key in employees) {
+    if (employees[key] === bestStats) {
+      bestEmployeeName = key;
+    }
+  }
+  return bestEmployeeName;
+};
+
+// Variant 2
+// const findBestEmployee = function (employees) {
+//   let bestStats = 0;
+//   let bestEmployeeName;
+
+//   if (Object.keys(employees).length === 0) {
+//     return "";
+//   }
+
+//   for (const key in employees) {
+//     if (bestStats < employees[key]) {
+//       bestStats = employees[key];
+//       bestEmployeeName = key;
+//     }
+//   }
+
+//   return bestEmployeeName;
+// };
+
+// Объекты и ожидаемый результат
+const developers = {
+  ann: 29,
+  david: 35,
+  helen: 1,
+  lorence: 99,
+};
+console.log(findBestEmployee(developers));
+// 'lorence'
+
+const supports = {
+  poly: 12,
+  mango: 17,
+  ajax: 4,
+};
+console.log(findBestEmployee(supports));
+// 'mango'
+
+const sellers = {
+  lux: 147,
+  david: 21,
+  kiwi: 19,
+  chelsy: 38,
+};
+console.log(findBestEmployee(sellers));
+// 'lux'
+
+// const empty = {};
+// console.log(findBestEmployee(empty));
--- js/task-4.js
+++ js/task-4.js
@@ -1,3 +1,34 @@
 console.log(
   "Task 4 ------------------------------------------------------------"
 );
+const countTotalSalary = function (employees) {
+  const totalArray = Object.values(employees);
+  let totalSalary = 0;
+
+  for (const item of totalArray) {
+    totalSalary += item;
+  }
+
+  return totalSalary;
+};
+
+// Объекты и ожидаемый результат
+const developers = {
+  mango: 300,
+  poly: 250,
+  alfred: 450,
+};
+console.log(countTotalSalary(developers));
+// 1000
+
+const supports = {
+  kiwi: 200,
+  lux: 150,
+  chelsy: 150,
+};
+console.log(countTotalSalary(supports));
+// 500
+
+const empty = {};
+console.log(countTotalSalary(empty));
+// 0
